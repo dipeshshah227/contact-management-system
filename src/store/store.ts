83bb1f45a4109dea0b91ec59a08375c3d7e2ab5e
dipeshshah227 <dipesh@appboxtech.com>
2023-05-22 21:54:28 +0545

    updated redux config

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authslice from "../slice/authslice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     authentication: authslice,
   },
 });
-export default store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
