dfd8ef3b876af5e3433595b59f731d5ec6d284d5
dipeshshah227 <dipesh@appboxtech.com>
2023-05-22 21:51:11 +0545

    config file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "input-appear": "input-appear 0.5s",
+      },
+      keyframes: {
+        "input-appear": {
+          "0%": {
+            transform: "translateY(100%)",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
